2a3cc5f451489f665442df0cd3f779bcc156a223
Senga <14marcel.senga@gmail.com>
2023-01-05 01:50:30 +0100

    Complete video card ui

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -9,7 +9,14 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card sx={{ background: "#333" }}>
+    <Card
+      sx={{
+        background: "#333",
+        width: { md: "240px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: "12px",
+      }}
+    >
       <Link to={videoId && `/videos/${videoId}`}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
@@ -22,6 +29,33 @@ const VideoCard = ({
           title={snippet.title}
         />
       </Link>
+      <CardContent sx={{ height: "100px", pl: 0 }}>
+        <Link to={videoId && `/videos/${videoId}`}>
+          <Typography
+            sx={{
+              fontSize: "0.9rem",
+              fontWeight: "400",
+              color: "#dfdfdf",
+              marginBottom: 1,
+              lineHeight: "1.2rem",
+            }}
+            title={snippet.title}
+          >
+            {snippet.title.length > 45
+              ? snippet.title.slice(0, 45) + "..."
+              : snippet.title}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId && `/channels/${snippet?.channelId}`}>
+          <Typography
+            sx={{ fontWeight: "300", color: "gray", fontSize: "0.8rem" }}
+            title={snippet.channelTitle}
+          >
+            {snippet?.channelTitle.slice(0, 40)}
+            <CheckCircle sx={{ color: "gray", fontSize: "13px", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
